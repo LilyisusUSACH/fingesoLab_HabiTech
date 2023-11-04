e1463af1952df49d85df9618630394320dc29fc6
Alejandro J. Cortes <alejandro.cortes.c@usach.cl>
2023-11-04 12:25:26 -0300

    [Alejandro cortes] Backend - creacion del proyecto

---
+++ HabiTech_Backend/src/main/java/com/IndustrialesComunes/HabiTech/HabiTechApplication.java
@@ -0,0 +1,13 @@
+package com.IndustrialesComunes.HabiTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HabiTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HabiTechApplication.class, args);
+	}
+
+}
---
+++ HabiTech_Backend/src/test/java/com/IndustrialesComunes/HabiTech/HabiTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IndustrialesComunes.HabiTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabiTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
